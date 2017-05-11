9ab71a8b6a37a1199836987e6102faa8d826aeb0
undef1ned <the.cooljames@gmail.com>
2017-05-12 01:06:33 +0300

    Rounding image dimensions

--- src/FitImage.js
+++ src/FitImage.js
@@ -8,33 +8,25 @@ import  {
 
 export default class FitImage extends Component {
 
-    state = {
-        height: 0,
-        width: 0
-    }
-
-    componentDidMount(){
-        Image.getSize(this.props.source.uri, (imageWidthPx, imageHeightPx) => {
-            const screenWidthPt = Dimensions.get('window').width
-
-            let newImageWidth = (imageWidthPx >= screenWidthPt) ?
-                screenWidthPt : imageWidthPx
-            let newImageHeight = (imageHeightPx / imageWidthPx) * newImageWidth
-            this.setState({
-                width: Math.round(newImageWidth),
-                height: Math.round(newImageHeight)
-            })
-        })
-
+    shouldComponentUpdate(){
+        return false
     }
 
     render() {
+        const imageHeightPx = this.props.originalHeight
+        const imageWidthPx = this.props.originalWidth
+        const screenWidthPt = Dimensions.get('window').width
+
+        let newImageWidth = (imageWidthPx >= screenWidthPt) ?
+            screenWidthPt : imageWidthPx
+        let newImageHeight = (imageHeightPx / imageWidthPx) * newImageWidth
+
         return (
             <Image
                 source={this.props.source}
                 style={{
-                    width: this.state.width,
-                    height: this.state.height
+                    width: Math.round(newImageWidth),
+                    height: Math.round(newImageHeight)
                 }}
             />
         )
